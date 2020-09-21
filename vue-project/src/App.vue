81baa589075ad02b99019329be00a3d981252249
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2020-09-21 15:48:21 +0300

    Update App.vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
     <Header></Header>
-    <router-view></router-view>
+    <router-view name="header-top"></router-view>
+    <transition name="slide" mode="out-in">
+      <router-view></router-view>
+    </transition>
     <Footer></Footer>
   </div>
 </template>
@@ -13,10 +16,46 @@ export default {
   name: "App",
   components: {
     Header,
-    Footer
-  }
+    Footer,
+  },
 };
 </script>
 
 <style>
+.slide-enter {
+  opacity: 0;
+}
+
+.slide-enter-active {
+  animation: slide-in 1s ease-out forwards;
+  transition: opacity 0.5s;
+}
+
+.slide-leave {
+}
+
+.slide-leave-active {
+  animation: slide-out 1s ease-out forwards;
+  transition: opacity 1s;
+  opacity: 0;
+  position: relative;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(20px);
+  }
+  to {
+    transform: translateY(0px);
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0px);
+  }
+  to {
+    transform: translateY(20px);
+  }
+}
 </style>
